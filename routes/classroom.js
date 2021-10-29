80a107cfae5231800083d05ec0a0b4605b497bda
atomic-Samurai <akshatkumar6932@gmail.com>
2021-10-29 18:50:19 +0530

    delete announcement option added for teacher

--- routes/classroom.js
+++ routes/classroom.js
@@ -8,4 +8,5 @@ const classroomController = require('../controllers/classroom_controller');
 router.get('/',passport.checkAuthentication,classroomController.openClassroom);
 router.post('/make-announcement',classroomController.makeAnnouncement);
 
+router.get('/delete-post',classroomController.deleteAnnouncement);
 module.exports = router;
